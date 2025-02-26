33384daf31b1a6b212d6368bd2648609f1826907
Dog Player <evgeniy.belov@softline.com>
2025-02-26 11:31:55 +0200

    Convert tabs to spaces

--- ConsoleApps/LeetCode/AsyncQueueWorker.cs
+++ ConsoleApps/LeetCode/AsyncQueueWorker.cs
@@ -8,51 +8,51 @@ using System.Threading.Tasks;
 namespace ConsoleApps.LeetCode
 {
     public class AsyncQueueWorker
-	{
-		private readonly Queue<Task> _taskQueue;
-		private readonly List<Task> _currentTasks;
-		private readonly object _lock = new object(); 
-		private readonly int _maxConcurrentTasks;
+    {
+        private readonly Queue<Task> _taskQueue;
+        private readonly List<Task> _currentTasks;
+        private readonly object _lock = new object(); 
+        private readonly int _maxConcurrentTasks;
 
-		public AsyncQueueWorker(int maxConcurrentTasks)
-		{
-		    _maxConcurrentTasks = maxConcurrentTasks;
-		    _taskQueue = new Queue<Task>(); 
-		    _currentTasks = new List<Task>();
-		}
+        public AsyncQueueWorker(int maxConcurrentTasks)
+        {
+            _maxConcurrentTasks = maxConcurrentTasks;
+            _taskQueue = new Queue<Task>(); 
+            _currentTasks = new List<Task>();
+        }
 
-		public async Task StartQueue()
-		{
-		    while (_currentTasks.Count > 0)
-		    {
-		        var completedTask = await Task.WhenAny(_currentTasks);
-		        _currentTasks.Remove(completedTask); 
+        public async Task StartQueue()
+        {
+            while (_currentTasks.Count > 0)
+            {
+                var completedTask = await Task.WhenAny(_currentTasks);
+                _currentTasks.Remove(completedTask); 
 
-				// Process the next task from the queue if there's room for it
-				lock (_lock)
-				{
-					if (_taskQueue.Count > 0)
-					{
-						var task = _taskQueue.Dequeue(); 
-						task.Start();
-						_currentTasks.Add(task);
-					}
-				}
-			}
-		}
+                // Process the next task from the queue if there's room for it
+                lock (_lock)
+                {
+                    if (_taskQueue.Count > 0)
+                    {
+                        var task = _taskQueue.Dequeue(); 
+                        task.Start();
+                        _currentTasks.Add(task);
+                    }
+                }
+            }
+        }
 
-		public void AddTaskInQueue(Task task)
-		{
-			if (_currentTasks.Count >= _maxConcurrentTasks)
-			{
-				lock (_lock)
-				{
-					_taskQueue.Enqueue(task);
-				}
-				return;
-			}
-			task.Start();
-			_currentTasks.Add(task);
-		}
-	}
+        public void AddTaskInQueue(Task task)
+        {
+            if (_currentTasks.Count >= _maxConcurrentTasks)
+            {
+                lock (_lock)
+                {
+                    _taskQueue.Enqueue(task);
+                }
+                return;
+            }
+            task.Start();
+            _currentTasks.Add(task);
+        }
+    }
 }
--- ConsoleApps/LeetCode/FindKthLargest.cs
+++ ConsoleApps/LeetCode/FindKthLargest.cs
@@ -6,62 +6,62 @@ using System.Threading.Tasks;
 
 namespace ConsoleApps.LeetCode
 {
-	//215. Kth Largest Element in an Array 
+    //215. Kth Largest Element in an Array 
     public partial class Solution
-	{
-		public int FindKthLargest(int[] nums, int k)
-		{
-			var heap = new Heap(k);
-			for (int i = 0; i < nums.Length; i++)
-			{
-				heap.Add(nums[i]);
-			}
+    {
+        public int FindKthLargest(int[] nums, int k)
+        {
+            var heap = new Heap(k);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                heap.Add(nums[i]);
+            }
 
-			return heap.Peek();
-		}
-		
-		private class Heap
-		{
-			private List<int> heap;
-			private int capacity;
-			
-			public Heap(int _capacity)
-			{
-				heap = new List<int>();
-				capacity = _capacity;
-			}
-			
-			public void Add(int val)
-			{
-				if (heap.Count == 0)
-				{
-					heap.Add(val);
-					return;
-				}
-				else if(val <= heap[0]  && heap.Count == capacity) return;
+            return heap.Peek();
+        }
+        
+        private class Heap
+        {
+            private List<int> heap;
+            private int capacity;
+            
+            public Heap(int _capacity)
+            {
+                heap = new List<int>();
+                capacity = _capacity;
+            }
+            
+            public void Add(int val)
+            {
+                if (heap.Count == 0)
+                {
+                    heap.Add(val);
+                    return;
+                }
+                else if(val <= heap[0]  && heap.Count == capacity) return;
 
-				int left = 0, right = heap.Count, mid = 0;
-				while (left <= right)
-				{
-					mid = left + (right - left) / 2;
-					
-					if(mid == heap.Count) break;
-					else if (heap[mid] < val) left = mid + 1;
-					else if (heap[mid] > val) right = mid - 1;
-					else
-					{
-						break;
-					}
-				}
-				heap.Insert(Math.Max(mid, left), val);
-				
-				if(heap.Count > capacity) heap.RemoveAt(0);
-			}
-			
-			public int Peek()
-			{
-				return heap[0];
-			}
-		}
-	}
+                int left = 0, right = heap.Count, mid = 0;
+                while (left <= right)
+                {
+                    mid = left + (right - left) / 2;
+                    
+                    if(mid == heap.Count) break;
+                    else if (heap[mid] < val) left = mid + 1;
+                    else if (heap[mid] > val) right = mid - 1;
+                    else
+                    {
+                        break;
+                    }
+                }
+                heap.Insert(Math.Max(mid, left), val);
+                
+                if(heap.Count > capacity) heap.RemoveAt(0);
+            }
+            
+            public int Peek()
+            {
+                return heap[0];
+            }
+        }
+    }
 }
